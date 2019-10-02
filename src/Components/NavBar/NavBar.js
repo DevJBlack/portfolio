000c62114aab65ee4c7ba999675f7c920ff55f3f
Devin Black <Devinjamesblack@gmail.com>
2019-10-02 09:42:18 -0600

    NavBar changes

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.scss'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const NavBar = () => {
+ const [edit, setEdit] = useState(true)
 
+ const displayEdit = () => {
+    if (edit === true) { 
+      return(setEdit(false))
+    } else {
+      return (
+        setEdit(true)
+      )
+    }
+ }
 
   return (
     <div>
       <nav className="navigation">
-          <AnchorLink offset='10'href='#Me'><p id='skills'>About Me</p></AnchorLink>
-          <AnchorLink offset='10'href='#Skills'><p id='skills'>Skills</p></AnchorLink>
+        { edit ?  
+        <div>
+          <label onClick={() => displayEdit(false)} htmlFor="toggle" className="label">&#9776;</label>
+          <input type="checkbox" id="toggle"/>
+        </div> 
+        :
+        <div className='menuPage'>
+            <label onClick={() => displayEdit(true)}>X</label>
+          <div className='menu'>
+            <AnchorLink offset='10'href='#Me'><p id='skills'>About Me</p></AnchorLink>
+            <AnchorLink offset='10'href='#Skills'><p id='skills'>Skills</p></AnchorLink>
+          </div>
+        </div>
+      }
       </nav>
     </div>
   )
