10a45cbcfdf9984fa07c63d2d7b93975d4bc55aa
Devin Black <Devinjamesblack@gmail.com>
2019-10-06 21:35:55 -0600

    new route path

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import frontPageTesla from '../Assests/HOMETESLAPROJECT.PNG'
 import dashBoard from '../Assests/Dashboard.png'
 import './Projects.scss'
+import ManagerAppHome from '../Assests/ManagerAppHome.PNG'
 
 const Projects = () => {
 
@@ -10,7 +11,9 @@ const Projects = () => {
 
   return (
     <div className='projectContainer'>
-      <h2>Projects</h2>
+      <div className='titleProject'>
+        <h2>Projects</h2>
+      </div>
       <div className='teslaProject'>
         <p> Tesla Clone </p>
         <img width='250' src={frontPageTesla} alt='teslaClone'/>
@@ -20,9 +23,16 @@ const Projects = () => {
       </div>
       <div className='btlsrvProject'>
         <p> Btlsrv </p>
-        <img width='250' src={dashBoard} alt='dashBoard'/>
+        <img height='130' width='250' src={dashBoard} alt='dashBoard'/>
         <div>
           <Link to='/btlsrv'><button>View Project</button></Link>
+        </div>
+      </div>
+      <div className='managerAppProject'>
+        <p> ManagerApp </p>
+        <img width='250' src={ManagerAppHome} alt='Home app'/>
+        <div>
+          <Link to='/managerapp'><button>View Project</button></Link>
         </div>
       </div>
     </div>
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Components/Home/Home'
 import TeslaClone from './Components/ViewProjects/Tesla/TeslaClone'
 import Btlsrv from './Components/ViewProjects/Btlsrv/Btlsrv'
+import ManagerApp from './Components/ViewProjects/ManagerApp/ManagerApp'
 
 export default (
   <Switch>
     <Route path='/' exact component={Home} />
     <Route path='/teslaclone' component={TeslaClone}/>
+    <Route path='managerapp' component={ManagerApp}/>
     <Route path='/btlsrv' component={Btlsrv}/>
   </Switch>
 )
