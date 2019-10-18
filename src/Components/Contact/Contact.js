ae7b0581a89393d9efa55d46f68ed7c50b73dcb2
Devin Black <Devinjamesblack@gmail.com>
2019-10-18 17:39:07 -0600

    contact & projects done

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,15 +6,15 @@ const Contact = () => {
   return(
     <div>
       <div className='titlePage'>
+        <div className='titleContainer'> 
         <div className='titleContact'>
           <h2>Contact Me</h2>
         </div>
-        {/* <div className='titleContainer'>  */}
-          <label className='phoneContact'>Phone: 385-535-5288 </label>
-          <label className='emailContact'>Email: Devinjamesblack@gmail.com </label>
-          <label className='linkedInContact'> LinkedIn:<a href='https://www.linkedin.com/in/devin-black/' rel='noopener noreferrer' target='_blank'> https://www.linkedin.com/in/devin-black/</a></label>
-          <label className='nameContact'>Phone: 385-535-5288 </label>
-        {/* </div> */}
+          <label className='phoneContact'>Phone: <span>385-535-5288</span></label>
+          <label className='emailContact'>Email:<span>Devinjamesblack@gmail.com</span></label>
+          <label className='linkedInContact'> LinkedIn:<a href='https://www.linkedin.com/in/devin-black/' rel='noopener noreferrer' target='_blank'><span>https://www.linkedin.com/in/devin-black</span></a></label>
+          <label className='gitHubContact'> GitHub:<a href='https://github.com/DevJBlack' rel='noopener noreferrer' target='_blank'><span>https://github.com/DevJBlack</span></a></label>
+        </div>
       </div>
     </div>
   )
